2a735ff98b01fd3628ac8ab21d0f405e443507de
avinashtare <avinashtare5@gmail.com>
2023-12-03 10:40:21 +0530

    refrence link added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 import styles from './page.module.css'
 
 export default function Home() {
+  const htmlCode = `<img src='/image'></img>`;
   return (
     <main className={styles.main}>
+
       <div className={styles.description}>
+        {/* ignore this code  */}
+        <p>
+          <code>
+            {htmlCode}
+          </code>
+          {/* it's single url to load multiple images */}
+          <a href='/image' target='_blank' className={styles.a}>Open Image Url to new tab</a>
+        </p>
         <p>
           <img src='/image'></img>
         </p>
